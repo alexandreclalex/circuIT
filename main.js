7b07f721d9441080dd0665d239f8d9a0529b2cba
Alexandre Clavel <Alexandre.Clavel.2000@gmail.com>
2022-04-05 23:21:46 -0500

    Pushing prototype code

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const webpageDir =  `${__dirname}/srv`;
 
+
 app.use(express.urlencoded({ extended: true }));
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running at http://localhost:3000");
