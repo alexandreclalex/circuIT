7e3ff7318cca7a806767de8ae590686528e92681
Alexandre Clavel <Alexandre.Clavel.2000@gmail.com>
2022-04-12 03:03:05 -0500

    Pushing final code

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const webpageDir =  `${__dirname}/srv`;
 
-
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', './srv')
 app.use(express.urlencoded({ extended: true }));
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running at http://localhost:3000");
@@ -11,7 +13,12 @@ app.listen(process.env.PORT || 3000, () => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(webpageDir))
 
+app.get('/game', (req, res) =>{
+    let index = req.query.level - 1;
+    res.render(`${webpageDir}/game`, {ind:index});
+})
+
 app.get('/', (request, response) => {
-    response.sendFile(`${webpageDir}/circuIT.html`);
+    response.sendFile(`${webpageDir}/index.html`);
 });
 
